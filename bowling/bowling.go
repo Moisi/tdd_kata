29e28cece29727d09b320b0e383e5529b1542a0e
Ohad Moisi <ekrembo@gmail.com>
2022-09-15 02:00:38 +0300

    roll; hack spare state and calc. should refactorw
    Signed-off-by: Ohad Moisi <ekrembo@gmail.com>

--- bowling/bowling.go
+++ bowling/bowling.go
@@ -4,11 +4,15 @@ import (
 	"fmt"
 )
 
+const NIL_PREVROLL = 100
+
 type Game struct {
 	GameName    string
 	FrameNumber uint
 	RollNumber  uint
 	Score       uint
+	IsSpare     bool
+	PrevRoll    uint
 }
 
 func (e Game) printGame() {
@@ -16,12 +20,35 @@ func (e Game) printGame() {
 }
 
 func newGame(GameName string) Game {
-	g := Game{GameName, 0, 0, 0}
+	g := Game{GameName, 0, 0, 0, false, NIL_PREVROLL}
+	g.IsSpare = false
 	return g
 }
 
 func (e *Game) roll(NumOfPinsDown uint) {
+	// calc current run
 	e.Score += NumOfPinsDown
+
+	// add bonus if prev was spare
+	if e.IsSpare {
+		// add the run after spare
+		e.Score += NumOfPinsDown
+		e.IsSpare = false
+	}
+
+	// prepare state for next roll:
+	if NumOfPinsDown+e.PrevRoll == 10 {
+		// congrats we have a spare:
+		e.IsSpare = true
+		e.PrevRoll = NIL_PREVROLL
+	} else if e.PrevRoll == NIL_PREVROLL {
+		// PrevRoll == NIL_PREVROLL == 100, meaning this is first roll in frame.
+		// need to save it for second roll
+		// might do this in a different way (via some state) but for now.
+		e.PrevRoll = NumOfPinsDown
+	} else {
+		e.PrevRoll = NIL_PREVROLL
+	}
 }
 
 func (e *Game) score() uint {
