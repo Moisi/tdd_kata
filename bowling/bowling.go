6bd0ca58b522497e4212db60a74ec0bd70649f95
Ohad Moisi <ekrembo@gmail.com>
2022-09-15 00:53:56 +0300

    add test: sanity for score after construction
    Signed-off-by: Ohad Moisi <ekrembo@gmail.com>

--- bowling/bowling.go
+++ bowling/bowling.go
@@ -15,7 +15,6 @@ func (e Game) printGame() {
 	fmt.Printf("%s %d %d %d\n", e.GameName, e.FrameNumber, e.RollNumber, e.Score)
 }
 
-
 func newGame(GameName string) Game {
 	g := Game{GameName, 0, 0, 0}
 	return g
--- bowling/bowling_test.go
+++ bowling/bowling_test.go
@@ -8,3 +8,14 @@ func TestGameInit(t *testing.T) {
 	g := newGame("tmpName")
 	g.printGame()
 }
+
+func TestScoreOfFreshGame(t *testing.T) {
+	// this is bad practice of TDD. because this test never failed first
+	g := newGame("ScoreOfFreshGame")
+	want := uint(0)
+	score := g.score()
+	if score != want {
+		t.Fatalf("ScoreOfFreshGame = %d, want = %d", score, want)
+	}
+}
+
