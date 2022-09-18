dc4142afadb9e960f5a98a47527540af2ab1dc8a
Ohad Moisi <ekrembo@gmail.com>
2022-09-18 03:32:41 +0300

    redesign data scheme to allow correct score calculations
    Signed-off-by: Ohad Moisi <ekrembo@gmail.com>

--- bowling/bowling.go
+++ bowling/bowling.go
@@ -7,11 +7,12 @@ import (
 const NIL_PREVROLL = 100
 
 type Frame struct {
-	Rolls [3]int
+	Rolls           [3]int
+	FinalScoreCache int
 }
 
 func newFrame() Frame {
-	f := Frame{[3]int{0, 0, 0}}
+	f := Frame{[3]int{0, 0, 0}, -1}
 	return f
 }
 
@@ -35,11 +36,11 @@ type Game struct {
 	FrameNumber uint
 	RollNumber  uint
 	Score       uint
-	IsSpare     bool
-	IsStrike    bool
+	FrameIndex  int
+	RollIndex   int
 	PrevRoll    uint
 
-	FrameArray [10]Frame
+	FramesArray [10]Frame
 }
 
 func (e Game) printGame() {
@@ -47,49 +48,72 @@ func (e Game) printGame() {
 }
 
 func newGame(GameName string) Game {
-	g := Game{GameName, 0, 0, 0, false, false, NIL_PREVROLL,
+	g := Game{GameName, 0, 0, 0, 0, 0, NIL_PREVROLL,
 		[10]Frame{newFrame(), newFrame(), newFrame(),
 			newFrame(), newFrame(), newFrame(),
 			newFrame(), newFrame(), newFrame(),
 			newFrame()}}
-	g.IsSpare = false
 	return g
 }
 
 func (e *Game) roll(NumOfPinsDown uint) {
-	// calc current run
-	e.Score += NumOfPinsDown
-
-	// add bonus if prev was spare
-	if e.IsSpare {
-		// add the run after spare
-		e.Score += NumOfPinsDown
-		e.IsSpare = false
-	} else if e.IsStrike {
-		e.Score += NumOfPinsDown
+	if e.FrameIndex == 10 || (e.FrameIndex == 9 && e.RollIndex == 2) {
+		return
 	}
 
-	// prepare state for next roll:
-	if NumOfPinsDown+e.PrevRoll == 10 {
-		// congrats we have a spare:
-		e.IsSpare = true
-		e.IsStrike = false
-		e.PrevRoll = NIL_PREVROLL
-	} else if e.PrevRoll == NIL_PREVROLL && NumOfPinsDown == 10 {
-		// found a strike:
-		e.IsStrike = true
-	} else if e.PrevRoll == NIL_PREVROLL {
-		// PrevRoll == NIL_PREVROLL == 100, meaning this is first roll in frame.
-		// need to save it for second roll
-		// might do this in a different way (via some state) but for now.
-		e.PrevRoll = NumOfPinsDown
+	currentFrame := e.FramesArray[e.FrameIndex]
+	e.FramesArray[e.FrameIndex].Rolls[e.RollIndex] = int(NumOfPinsDown)
+
+	if e.FrameIndex == 9 && currentFrame.Rolls[0]+currentFrame.Rolls[1] == 10 {
+		// last frame + strike | spare
+		e.RollIndex = 2
+	} else if e.RollIndex == 0 && NumOfPinsDown == 10 {
+		// strike, jump to next Frame
+		e.FrameIndex++
+		e.RollIndex = 0
+	} else if e.RollIndex == 1 {
+		e.FrameIndex++
+		e.RollIndex = 0
 	} else {
-		// this was second roll. make sure IsStrike = false;
-		e.IsStrike = false
-		e.PrevRoll = NIL_PREVROLL
+		e.RollIndex = 1
 	}
 }
 
+func (e *Game) getBonus(i int, SorS bool) int {
+	if i == 10 {
+		// this is last frame, we store next roll in:
+		// Rolls[2]:
+		return e.FramesArray[i-1].Rolls[2]
+	} else {
+		f := e.FramesArray[i]
+		if SorS {
+			// strike - return sum
+			// of next two rolls
+			if f.Rolls[0] == 10 {
+				// next two rolls are in two frames:
+				return 10 + e.getBonus(i+1, false)
+			} else {
+				// two are in same frame:
+				return f.Rolls[0] + f.Rolls[1]
+			}
+		} else {
+			// spare - return next roll
+			return f.Rolls[0]
+		}
+	}
+
+}
+
 func (e *Game) score() uint {
-	return e.Score
+	score := 0
+	for i, f := range e.FramesArray {
+		score += f.Rolls[0]
+		score += f.Rolls[1]
+		if f.Rolls[0] == 10 {
+			score += e.getBonus(i+1, true)
+		} else if f.Rolls[0]+f.Rolls[1] == 10 {
+			score += e.getBonus(i+1, false)
+		}
+	}
+	return uint(score)
 }
